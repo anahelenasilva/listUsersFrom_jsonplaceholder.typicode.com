6edca5dc2e23d2597bf807073b4aacaad9d88f02
Ana Helena <anahelenarp@hotmail.com>
2019-05-18 10:10:46 -0300

    Add code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h3>Listar usuários de https://jsonplaceholder.typicode.com/users</h3>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      url: "https://jsonplaceholder.typicode.com/users",
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(this.state.url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(result) {
+        console.log(result);
+        this.setState({ users: result });
+      });
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div className="App">
+        <h3>List users from https://jsonplaceholder.typicode.com/users</h3>
+
+        <div>
+          {users.map(data => {
+            return (
+              <div>
+                <p>data.id</p>
+                <p>data.name</p>
+                <p>data.email</p>
+                <p>data.address.street</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
